91a08533a8c05683ca8332bec188703bef64c80e
Gabe Albacarys <g.albacarys@gmail.com>
2021-12-01 01:30:04 +0000

    linux fixes

--- .tmux/batt.sh
+++ .tmux/batt.sh
@@ -3,6 +3,7 @@
 # Fail out for other OSes
 if [ -z $(echo $OSTYPE | grep "darwin") ]; then
 	echo "??%"
+	exit 0
 fi
 
 percent=$(pmset -g batt | grep [0-9][0-9]% | awk 'NR==1{print$3}' | cut -c1-3)
